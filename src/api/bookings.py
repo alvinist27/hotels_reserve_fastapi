cb72ae5b084d61e83d635f6b3617893a35dd1fc2
hotamov <s.khotamov@yadro.com>
2025-08-04 23:52:39 +0300

    Добавлен просмотр бронирований

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,19 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from src.api.dependencies import DBDep, UserIDDep
-from src.schemas.booking import BookingRequestAddSchema, BookingSchema, BookingAddSchema
+from src.schemas.booking import BookingAddSchema, BookingRequestAddSchema, BookingSchema
 
 bookings_router = APIRouter(prefix='/bookings', tags=['Bookings'])
 
 
 @bookings_router.get('/')
+async def get_bookings(db: DBDep, _: UserIDDep) -> list[BookingSchema]:
+    return await db.bookings.get_all()
+
+
+@bookings_router.get('/me')
 async def get_user_bookings(db: DBDep, user_id: UserIDDep) -> list[BookingSchema]:
-    return await db.bookings.get_all(user_id=user_id)
+    return await db.bookings.get_filtered(user_id=user_id)
 
 
 @bookings_router.post('/')
 async def get_user_bookings(db: DBDep, user_id: UserIDDep, booking_data: BookingRequestAddSchema) -> dict:
-    create_data = BookingAddSchema(**booking_data.model_dump(), user_id=user_id)
+    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    if not room:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Room with specifed room_id not found')
+    create_data = BookingAddSchema(**booking_data.model_dump(), user_id=user_id, price=room.price)
     booking = await db.bookings.add(create_data)
     await db.commit()
     return {'status': 'OK', 'data': booking}
--- src/schemas/booking.py
+++ src/schemas/booking.py
@@ -5,12 +5,12 @@ from datetime import date
 class BookingRequestAddSchema(BaseModel):
     date_from: date = Field(description='Дата заезда')
     date_to: date = Field(description='Дата выезда')
-    price: int = Field(description='Цена бронирования')
     room_id: int = Field(description='ID комнаты')
 
 
 class BookingAddSchema(BookingRequestAddSchema):
     user_id: int = Field(description='ID пользователя')
+    price: int = Field(description='Цена бронирования')
 
 
 class BookingSchema(BookingAddSchema):
