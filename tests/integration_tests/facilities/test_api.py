24862c7878c3538a5c78efc600b6e617ba1b4e5e
hotamov <s.khotamov@yadro.com>
2025-08-22 16:15:56 +0300

    Добавлены тесты на CRUD API

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -1,8 +1,14 @@
 async def test_get_facilities(ac):
     response = await ac.get('/facilities/')
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
 
 async def test_create_facilities(ac):
-    response = await ac.post('/facilities/', json={'title': '123'})
+    facility_title = '123'
+    response = await ac.post('/facilities/', json={'title': facility_title})
     assert response.status_code == 201
+    res = response.json()
+    assert isinstance(res, dict)
+    assert 'data' in res
+    assert res['data']['title'] == facility_title
